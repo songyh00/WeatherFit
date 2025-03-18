2baa3bd6967a90ab9c24562d523a16d3a87a5084
SongYuHyeon <songyhyh00@gmail.com>
2025-03-18 20:35:03 +0900

    Initial commit for WeatherFit project

---
+++ WeatherFitBackend/src/main/java/com/weatherfit/backend/WeatherFitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.weatherfit.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherFitBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherFitBackendApplication.class, args);
+    }
+
+}
---
+++ WeatherFitBackend/src/test/java/com/weatherfit/backend/WeatherFitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weatherfit.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherFitBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
