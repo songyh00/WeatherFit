d7ba9528f9aa353e2529777b470a24c4816eed40
SongYuHyeon <songyhyh00@gmail.com>
2025-03-20 18:54:34 +0900

    Fix: Update styles and structure

---
+++ WeatherFitFrontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+// src/components/Navbar.jsx
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav>
+            <h1>WeatherFit</h1>
+        </nav>
+    );
+}
+
+export default Navbar;
