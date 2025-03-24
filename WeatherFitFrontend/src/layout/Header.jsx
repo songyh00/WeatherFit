ef914c87e74e4946d29d86041d6bfbd689f84e81
AnSangHyeon <ash040906@naver.com>
2025-03-24 15:39:02 +0900

    ë필요하지 않은 코드 삭제, 수정

--- WeatherFitFrontend/src/layout/Header.jsx
+++ WeatherFitFrontend/src/layout/Header.jsx
@@ -4,10 +4,10 @@ import {
     HeaderNav,
     HeaderWrapper,
     MainLogoLink,
-    MainLogoTitle,
     MainMenu,
     MainMenuLink,
     UserActions,
+    UserActionsContainer,
     UserActionsLink
 } from './index.style.js';
 import { Link } from 'react-router-dom';
@@ -21,22 +21,23 @@ const Header = () => {
         // styled 코드들은 src/layout/index.style.js 파일에 있습니다.
         <HeaderWrapper>
             <HeaderContent>
-                <UserActions>
-                    {/* 추후 라우팅 추가 예정 */}
-                    <UserActionsLink to="/">로그인</UserActionsLink> |&nbsp;
-                    <UserActionsLink to="/">회원가입</UserActionsLink> |&nbsp;
-                    <UserActionsLink to="/">마이페이지</UserActionsLink> |&nbsp;
-                    <UserActionsLink to="/">고객센터</UserActionsLink>
+                <UserActionsContainer>
+                    <MainLogoLink to="/">
+                        <img src={logo} alt="WeatherFit Logo" />
+                    </MainLogoLink>
 
-                </UserActions>
+                    <UserActions>
+                        {/* 추후 라우팅 추가 예정 */}
+                        <UserActionsLink to="/">로그인</UserActionsLink>|
+                        <UserActionsLink to="/">회원가입</UserActionsLink>|
+                        <UserActionsLink to="/">&nbsp;마이페이지</UserActionsLink>&nbsp;|
+                        <UserActionsLink to="/">고객센터</UserActionsLink>
+                    </UserActions>
+                </UserActionsContainer>
 
 
                 <HeaderNav>
-                    <MainLogoLink to="/">
-                        {/* MainLogo 컴포넌트와 로고 텍스트 */}
-                        {/* <MainLogo /> */}
-                        <img src={logo} alt="WeatherFit Logo" />
-                    </MainLogoLink>
+
 
                     <MainMenu>
                         {/* 임시 데이터 메뉴 */}
--- WeatherFitFrontend/src/layout/index.style.js
+++ WeatherFitFrontend/src/layout/index.style.js
@@ -3,9 +3,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const HeaderWrapper = styled.div`
-    width: 100%;
-    padding-bottom: 70px;
-    
+    //border-bottom: 1px solid #000;
 `;
 
 export const HeaderContent = styled.div`
@@ -15,41 +13,32 @@ export const HeaderContent = styled.div`
 
 export const UserActions = styled.div`
     display: flex;  // flex 사용 (가로 정렬사용하기위해)
-    height: 30px;
     line-height: 30px;
-    padding: 5px 0 30px 0;
     font-size: 10px;  
     color: #000;  
-    gap: 10px;  // 아이템 사이 간격
     justify-content: end;
-`;
-
-export const MainLogoTitle = styled.span`
-    font-size: 45px;
-    padding: 0 0 0 10px;
+    position: absolute;
+    right: 0;
+    top: 0;
 `;
 
 export const HeaderNav = styled.div`
     display: flex;
-    flex-direction: column; /* 세로 정렬 */
     align-items: center; /* 가운데 정렬 */
-    height: 10px;
     justify-content: center;
-    gap: 0px; /* 로고와 메뉴 사이 */
-    padding-top: 10px; /* 전체적으로 아래로 내림 */
-    
 `;
 
 export const UserActionsLink = styled(Link)`
     text-decoration: none;  // 링크의 기본 밑줄 제거
     color: #000; // 글자 색상 검은색으로 설정
+    width: 60px;
+    text-align: center;
+    font-size: 11px;
 `;
 
 export const MainLogoLink = styled(Link)`
     text-decoration: none;  // 링크의 기본 밑줄 제거
     color: #000; // 글자 색상 검은색으로 설정
-    display: flex;
-    align-items: center;
 `;
 
 export const MainMenu = styled.div`
@@ -62,9 +51,16 @@ export const MainMenuLink = styled(Link)`
     width: 150px;
     text-align: center;
     font-weight: bold;
-    font-size: 15px;
+    font-size: 16px;
     height: 50px;
-    line-height: 40px;
+    line-height: 50px;
+`;
+
+export const UserActionsContainer = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 
 
