b6a6d61060d83e0bf65583e93a239d305cd3bff2
dongryul2 <hdr5644@naver.com>
2025-03-24 14:52:36 +0900

    1헤더 레이아이웃 수정

--- WeatherFitFrontend/src/layout/Header.jsx
+++ WeatherFitFrontend/src/layout/Header.jsx
@@ -12,6 +12,8 @@ import {
 } from './index.style.js';
 import { Link } from 'react-router-dom';
 import MainLogo from "../components/MainLogo.jsx";
+import logo from "../assets/logo.png";
+
 
 const Header = () => {
     return (
@@ -21,28 +23,33 @@ const Header = () => {
             <HeaderContent>
                 <UserActions>
                     {/* 추후 라우팅 추가 예정 */}
-                    <UserActionsLink to="/">이용안내</UserActionsLink> |&nbsp;
-                    <UserActionsLink to="/">고객센터</UserActionsLink> |&nbsp;
                     <UserActionsLink to="/">로그인</UserActionsLink> |&nbsp;
-                    <UserActionsLink to="/">회원가입</UserActionsLink>
+                    <UserActionsLink to="/">회원가입</UserActionsLink> |&nbsp;
+                    <UserActionsLink to="/">마이페이지</UserActionsLink> |&nbsp;
+                    <UserActionsLink to="/">고객센터</UserActionsLink>
+
                 </UserActions>
+
 
                 <HeaderNav>
                     <MainLogoLink to="/">
                         {/* MainLogo 컴포넌트와 로고 텍스트 */}
-                        {<MainLogo />}
-                        <MainLogoTitle>WeatherFit</MainLogoTitle>
+                        {/* <MainLogo /> */}
+                        <img src={logo} alt="WeatherFit Logo" />
                     </MainLogoLink>
 
                     <MainMenu>
                         {/* 임시 데이터 메뉴 */}
-                        <MainMenuLink to="/">임시데이터1</MainMenuLink>
-                        <MainMenuLink to="/">임시데이터2</MainMenuLink>
-                        <MainMenuLink to="/">임시데이터3</MainMenuLink>
-                        <MainMenuLink to="/">임시데이터4</MainMenuLink>
-                        <MainMenuLink to="/">임시데이터5</MainMenuLink>
+                        <MainMenuLink to="/">BEST</MainMenuLink>
+                        <MainMenuLink to="/">오늘의 날씨</MainMenuLink>
+                        <MainMenuLink to="/">추천</MainMenuLink>
+                        <MainMenuLink to="/">아우터</MainMenuLink>
+                        <MainMenuLink to="/">상의</MainMenuLink>
+                        <MainMenuLink to="/">바지</MainMenuLink>
                     </MainMenu>
+
                 </HeaderNav>
+
             </HeaderContent>
 
 
--- WeatherFitFrontend/src/layout/index.style.js
+++ WeatherFitFrontend/src/layout/index.style.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 
 export const HeaderWrapper = styled.div`
     width: 100%;
-    height: 150px;
+    padding-bottom: 70px;
+    
 `;
 
 export const HeaderContent = styled.div`
@@ -17,7 +18,7 @@ export const UserActions = styled.div`
     height: 30px;
     line-height: 30px;
     padding: 5px 0 30px 0;
-    font-size: 14px;  
+    font-size: 10px;  
     color: #000;  
     gap: 10px;  // 아이템 사이 간격
     justify-content: end;
@@ -30,9 +31,13 @@ export const MainLogoTitle = styled.span`
 
 export const HeaderNav = styled.div`
     display: flex;
-    align-items: center;
-    height: 90px;
-    justify-content: space-between;
+    flex-direction: column; /* 세로 정렬 */
+    align-items: center; /* 가운데 정렬 */
+    height: 10px;
+    justify-content: center;
+    gap: 0px; /* 로고와 메뉴 사이 */
+    padding-top: 10px; /* 전체적으로 아래로 내림 */
+    
 `;
 
 export const UserActionsLink = styled(Link)`
@@ -57,9 +62,9 @@ export const MainMenuLink = styled(Link)`
     width: 150px;
     text-align: center;
     font-weight: bold;
-    font-size: 18px;
-    height: 90px;
-    line-height: 90px;
+    font-size: 15px;
+    height: 50px;
+    line-height: 40px;
 `;
 
 
