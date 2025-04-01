8276871cc3f15cefabeca13c292ebec2b67460d9
AnSangHyeon <ash040906@naver.com>
2025-04-01 11:02:47 +0900

    ㅎí회원가입 페이지에서도 푸í헤더 푸터 안보이게 설정해뒀습니다.

--- WeatherFitFrontend/src/pages/JoinTheMembership.jsx
+++ WeatherFitFrontend/src/pages/JoinTheMembership.jsx
@@ -3,12 +3,22 @@ import '../App.css';
 import {
     ContentsWrapper
 } from "../layout/mainPage.style.js";
+import {LoginButton, LoginInput, LoginSection, LoginWrapper} from "../layout/login.style.js";
+import {Link} from "react-router-dom";
+import logo from "../assets/logo.png";
 
 const JoinTheMembership = () => {
     return (
-        <ContentsWrapper>
-            회원가입 페이지 입니다.
-        </ContentsWrapper>
+        <RegisterWrapper
+            // action={} 어디로 전달해줘야할지 모르곘어서 일단 두겠습니다
+        >
+            <Link to="/">
+                <img src={logo} alt="WeatherFit Logo" />
+            </Link>
+            <LoginSection>
+
+            </LoginSection>
+        </RegisterWrapper>
     );
 }
 
