ef3814022957e688ceed8469a28f479011718ec9
light glow <119785026+LIGHTglow0704@users.noreply.github.com>
2025-04-01 16:09:57 +0900

    Revert "메인페이지 수정 04.01"

--- WeatherFitFrontend/src/pages/CustomerServiceCenter.jsx
+++ WeatherFitFrontend/src/pages/CustomerServiceCenter.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 import '../App.css';
 import {
     ContentsWrapper
-} from "../layout/CustomerServiceCenter.style.js";
+} from "../layout/mainPage.style.js";
 
 const CustomerServiceCenter = () => {
     return (
         <ContentsWrapper>
             고객센터 페이지 입니다.
         </ContentsWrapper>
-
-
     );
 }
 
