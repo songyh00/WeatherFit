1b8b0df07f1fe13e65ce40458b54cdc31121fda6
AnSangHyeon <ash040906@naver.com>
2025-05-20 13:42:41 +0900

    슬라이더 변경

---
+++ WeatherFitFrontend/src/components/SliderBtns.jsx
@@ -0,0 +1,35 @@
+export const MyCaretRightRectangle = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="40"       // 가로는 원래대로
+        height="40"      // 세로를 길게
+        fill="currentColor"
+        viewBox="0 0 16 16"
+        className="bi bi-caret-right-rectangle"
+    >
+        <path
+            d="M5.795 12.456A.5.5 0 0 1 5.5 12V4a.5.5 0 0 1 .832-.374l4.5 4a.5.5 0 0 1 0 .748l-4.5 4a.5.5 0 0 1-.537.082"
+        />
+        <path
+            d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"
+        />
+    </svg>
+);
+
+export const MyCaretLeftRectangle = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="46"
+        height="40"
+        fill="currentColor"
+        viewBox="0 0 16 16"
+        className="bi bi-caret-left-rectangle"
+    >
+        <path
+            d="M10.205 12.456A.5.5 0 0 0 10.5 12V4a.5.5 0 0 0-.832-.374l-4.5 4a.5.5 0 0 0 0 .748l4.5 4a.5.5 0 0 0 .537.082"
+        />
+        <path
+            d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"
+        />
+    </svg>
+);
